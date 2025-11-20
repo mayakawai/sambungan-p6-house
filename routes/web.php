03d2ef4a263c73bc051197d51abb88be8dce75c1
mayakawai <emirsyauqi23ti@mahasiswa.pcr.ac.id>
2025-11-20 08:30:22 +0700

    p11

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index']);
